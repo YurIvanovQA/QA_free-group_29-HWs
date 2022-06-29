45e666afd1ffee1ae8f4dcadc30defa386dddbb2
YurIvanovQA <yuri.ivanov.qa@gmail.com>
2022-06-29 15:53:11 +0600

    1

--- hw_js/hw_js.js
+++ hw_js/hw_js.js
@@ -180,35 +180,35 @@ checkAge_2("48fff")
 // Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
 // let age = 10, age_2 = 18, age_3 = 60;
 
-const checkAge_3 = function(age) {
+// const checkAge_3 = function(age) {
     
 
-    console.log(age)
+//     console.log(age)
 
-    // if(!isNaN (age) ) {
-            // if(Number(age) ) {
-                if(+age){
+//     // if(!isNaN (age) ) {
+//             // if(Number(age) ) {
+//                 if(+age){
 
-        if(age < age_2) {
-            console.log("You don’t have access cause your age is "  + age +  " It’s less then " + age_2)
-        }
-        else if(age>=age_2 && age<age_3) {
-            console.log("Welcome !")
-        }
-        else if(age>=age_3) {
-            console.log("Keep calm and look Culture channel")
-        }
-        else {
-            console.log("Technical work")
-        }
-    }
-else {
-    console.log('Error')}
-}
+//         if(age < age_2) {
+//             console.log("You don’t have access cause your age is "  + age +  " It’s less then " + age_2)
+//         }
+//         else if(age>=age_2 && age<age_3) {
+//             console.log("Welcome !")
+//         }
+//         else if(age>=age_3) {
+//             console.log("Keep calm and look Culture channel")
+//         }
+//         else {
+//             console.log("Technical work")
+//         }
+//     }
+// else {
+//     console.log('Error')}
+// }
 
-checkAge ( -17 )
-// // checkAge("61")
-// // checkAge("48fff")
+// checkAge ( -17 )
+// // // checkAge("61")
+// // // checkAge("48fff")
 
-let agePromt = prompt('Enter age');
-checkAge_3(agePromt);
+// let agePrompt = prompt('Enter age');
+// checkAge_3(agePrompt);
